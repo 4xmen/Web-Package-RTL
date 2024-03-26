98c11aac8c3f5773dafefbb39990f8b7e54f3855
YasinDehfuli <ydehfuli@gmail.com>
2024-03-26 17:42:59 +0330

    🆕 Add new mega-menu-4.js

---
+++ mega-menu-4/assets/js/mega-menu-4.js
@@ -0,0 +1,137 @@
+// Menu Script
+(function() {
+    "use strict";
+
+//    Selector Helper
+
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+//    Event Listener
+
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+//  Onscroll event listener
+
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+//    Navbar links
+
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+    // Scrolls to an element with header offset
+
+    const scrollto = (el) => {
+        let header = select('#header')
+        let offset = header.offsetHeight
+
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+        })
+    }
+
+    // Toggle .header-scrolled class to #header when page is scrolled
+
+    let selectHeader = select('#header')
+    let selectTopbar = select('#topbar')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+                if (selectTopbar) {
+                    selectTopbar.classList.add('topbar-scrolled')
+                }
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+                if (selectTopbar) {
+                    selectTopbar.classList.remove('topbar-scrolled')
+                }
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+//   Mobile nav toggle
+
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('ri-menu-2-line')
+        this.classList.toggle('ri-close-large-line')
+    })
+
+//     Mobile nav dropdown
+
+    on('click', '.navbar .dropdown > a', function(e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
+
+})()
+
+// Menu Script
+
+// Back to Top
+
+const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+}
+
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+        return [...document.querySelectorAll(el)]
+    } else {
+        return document.querySelector(el)
+    }
+}
+
+let backtotop = select('.back-to-top')
+if (backtotop) {
+    const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+            backtotop.classList.add('active')
+        } else {
+            backtotop.classList.remove('active')
+        }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+}
+
